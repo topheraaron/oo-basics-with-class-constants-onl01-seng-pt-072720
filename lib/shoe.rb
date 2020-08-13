7d2ef890ef2d7668b41cc9bd6333e2120be2e88d
Christopher Arias <chrstphr.aaron@gmail.com>
2020-08-13 04:24:51 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,6 +6,8 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
+    BRANDS << brand
+    BRANDS == BRANDS.uniq!
   end
 
   def cobble
@@ -13,10 +15,5 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
-  def brand=(brand)
-    @brand = brand
-    BRANDS << brand
-
-  end
 
 end
